9ea7c1ec91c3ffaac37126439536f845ea3ec797
Ian Blake <IanMBlake13@gmail.com>
2018-12-04 21:51:28 -0500

    Innitial commit with code and scene

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Graph  {
+
+	public int rows = 0, columns = 0;
+	public Node[] nodes;
+
+	public Graph(int[,] grid){
+
+		rows = grid.GetLength(0);
+		columns = grid.GetLength(1);
+
+		nodes = new Node[grid.Length];
+
+		for(int i = 0; i < nodes.Length; i++){
+			
+			Node node = new Node();
+			node.label = i.ToString();
+			nodes[i] = node;
+		}
+
+		for(int r = 0; r < rows; r++){
+			for(int c = 0; c < columns; c++){
+				Node node = nodes[columns*r +c];
+
+				if(grid[r,c] == 1){
+					continue;
+				}
+
+				//up
+				if(r > 0){
+					node.adjacent.Add(nodes[columns*(r-1)+c]);
+				}
+				
+				//right
+				if(c < columns-1){
+					node.adjacent.Add(nodes[columns*r+c+1]);
+				}
+
+				//down
+				if(r < rows-1){
+					node.adjacent.Add(nodes[columns*(r+1)+c]);
+				}
+
+				//left
+				if(c > 0){
+					node.adjacent.Add(nodes[columns*r+c-1]);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PathFindingTest.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFindingTest : MonoBehaviour {
+
+	public GameObject mapGroup;
+	// Use this for initialization
+	void Start () {
+		
+		int[,] map = new int[5, 5]{
+			{0,1,0,0,0},
+			{0,1,0,0,0},
+			{0,1,0,0,0},
+			{0,1,0,0,0},
+			{0,0,0,0,0}
+		};
+
+		Graph graph = new Graph(map);
+
+		Search search = new Search(graph);
+
+		search.Start(graph.nodes[0], graph.nodes[2]);
+
+		while(!search.finished){
+			search.Step();
+		}
+
+		print("Search done, path count is " + search.path.Count + " and iterations were " + search.iterations);
+
+		ResetMapGroup(graph);
+
+		foreach(Node node in search.path){
+			GetImage(node.label).color = Color.red;
+		}
+	}
+	
+	Image GetImage(string label){
+
+		int id = Int32.Parse(label);
+		GameObject square = mapGroup.transform.GetChild(id).gameObject;
+
+		return square.GetComponent<Image>();
+	}
+
+	void ResetMapGroup(Graph graph){
+
+		foreach(Node node in graph.nodes){
+			GetImage(node.label).color = node.adjacent.Count == 0 ? Color.white : Color.gray;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
